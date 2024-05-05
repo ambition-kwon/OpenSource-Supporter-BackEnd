4e5f9af2e036bca926f0e03217db91e169a9a5d2
ambition-kwon <prince5390@gmail.com>
2024-05-05 17:52:54 +0900

    [add] 모든 Entity에 modified_at column추가

--- src/main/java/me/jejunu/opensource_supporter/domain/GainedPoint.java
+++ src/main/java/me/jejunu/opensource_supporter/domain/GainedPoint.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -26,6 +27,9 @@ public class GainedPoint {
 
     @CreatedDate
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
 
     @ManyToOne
     @JoinColumn(name = "uid")
--- src/main/java/me/jejunu/opensource_supporter/domain/User.java
+++ src/main/java/me/jejunu/opensource_supporter/domain/User.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -40,6 +41,9 @@ public class User {
 
     @CreatedDate
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
     private List<RepoItem> repoItemList;
