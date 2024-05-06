d68adca0092aa42b6701fc029c6c19c7ba0d46b6
ambition-kwon <prince5390@gmail.com>
2024-05-06 18:51:58 +0900

    [modify] github에서 데이터 전달 시, json으로 받도록 수정

--- src/main/java/me/jejunu/opensource_supporter/config/GithubApiFeignClient.java
+++ src/main/java/me/jejunu/opensource_supporter/config/GithubApiFeignClient.java
@@ -2,11 +2,14 @@ package me.jejunu.opensource_supporter.config;
 
 import me.jejunu.opensource_supporter.dto.GithubAuthWithdrawalRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "GithubApi", url = "https://api.github.com")
 public interface GithubApiFeignClient {
-    @GetMapping(value = "/user")
+    @GetMapping(value = "/user",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     String getUser(@RequestHeader("Authorization") String authorization);
 
     @DeleteMapping(value = "/applications/{clientId}/grant")
--- src/main/java/me/jejunu/opensource_supporter/config/GithubAuthFeignClient.java
+++ src/main/java/me/jejunu/opensource_supporter/config/GithubAuthFeignClient.java
@@ -1,12 +1,15 @@
 package me.jejunu.opensource_supporter.config;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "GithubAuth", url = "https://github.com")
 public interface GithubAuthFeignClient {
-    @PostMapping(value = "/login/oauth/access_token")
+    @PostMapping(value = "/login/oauth/access_token",
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     String getAcessToken(@RequestParam("client_id") String clientId,
                          @RequestParam("client_secret") String clientSecret,
                          @RequestParam("code") String code);
