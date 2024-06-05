90b5e0887c58a039eb50c20888ba7643086a0ac2
ambition-kwon <prince5390@gmail.com>
2024-06-05 19:52:48 +0900

    [add] profile page api추가(유저 정보)

--- src/main/java/me/jejunu/opensource_supporter/controller/UserController.java
+++ src/main/java/me/jejunu/opensource_supporter/controller/UserController.java
@@ -1,6 +1,7 @@
 package me.jejunu.opensource_supporter.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.jejunu.opensource_supporter.domain.User;
 import me.jejunu.opensource_supporter.dto.CardInfoResponseDto;
 import me.jejunu.opensource_supporter.service.UserService;
 import org.springframework.http.ResponseEntity;
@@ -19,5 +20,11 @@ public class UserController {
     public ResponseEntity<CardInfoResponseDto> getCardInfo(@RequestParam("userName") String userName){
         CardInfoResponseDto cardInfoResponseDto = userService.getCardInfo(userName);
         return ResponseEntity.ok().body(cardInfoResponseDto);
+    }
+
+    @GetMapping("/api/user")
+    public ResponseEntity<User> getUser(@RequestParam("userName") String userName){
+        User user = userService.getUser(userName);
+        return ResponseEntity.ok().body(user);
     }
 }
