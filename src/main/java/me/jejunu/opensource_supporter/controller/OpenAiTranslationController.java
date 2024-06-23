1d48c660794fd55cb00af9cc44dd5dc169095425
ambition-kwon <prince5390@gmail.com>
2024-06-23 16:04:33 +0900

    [modify] Translate Logic(GET -> POST)

--- src/main/java/me/jejunu/opensource_supporter/controller/OpenAiTranslationController.java
+++ src/main/java/me/jejunu/opensource_supporter/controller/OpenAiTranslationController.java
@@ -6,7 +6,7 @@ import me.jejunu.opensource_supporter.service.OpenAiTranslationService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +19,7 @@ public class OpenAiTranslationController {
     @Value("${openai.api-key}")
     private String openApiKey;
 
-    @GetMapping("/api/translate")
+    @PostMapping("/api/translate")
     public ResponseEntity<String> translateText(@RequestBody TranslateRequestDto request){
         String response = openAiTranslationService.translateText(request, "Bearer " + openApiKey);
         return ResponseEntity.ok().body(response);
